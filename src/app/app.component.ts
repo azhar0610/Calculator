99dda59ab4ded24888587f014f694fd74b8b266e
Sahil <sahilansari5655@gmail.com>
2021-04-11 19:06:42 +0530

    Creating Home and  functiolity for + & - button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  operation !: string;
+
+  getOperation(operand : string){
+    this.operation = operand;
+  }
 }
