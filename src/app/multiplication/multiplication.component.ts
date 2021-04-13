de09dd3c4d7326758acb3df931b21fa851f165bc
Azhar <2020mt93127@wilp.bits-pilani.ac.in>
2021-04-13 09:51:16 +0530

    Add multiplication components

---
+++ src/app/multiplication/multiplication.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplication',
+  templateUrl: './multiplication.component.html',
+  styleUrls: ['./multiplication.component.css']
+})
+export class MultiplicationComponent implements OnInit {
+  val1!: any;
+  val2!: any;
+  val3!: any;
+  showError: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  subract(){
+    if(this.val1== null || this.val2== null) {
+      this.showError = true;
+    } else {
+      this.val3 = this.val1 * this.val2;
+      this.showError = false;
+    }
+    }
+
+
+}
